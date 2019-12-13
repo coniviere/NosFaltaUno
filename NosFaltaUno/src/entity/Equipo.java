32492735ab1c16f3519dd428914e5c23b8ed46f2
coniviere <43079502+coniviere@users.noreply.github.com>
2019-12-13 11:53:51 -0300

    update equipo

--- NosFaltaUno/src/entity/Equipo.java
+++ NosFaltaUno/src/entity/Equipo.java
@@ -1,18 +1,17 @@
 package entity;
 
-import java.util.ArrayList;
 
 public class Equipo {
 	
 	public String nombre;
 	public int posicionFaltante;
 	public String numeroCelular;
-	public ArrayList<String> mensajesRecibidos;
 
 
-	public Equipo(String nombre, int posicionFaltante) {
+	public Equipo(String nombre, int posicionFaltante, String num) {
 		this.nombre = nombre;
 		this.posicionFaltante = posicionFaltante;
+		this.numeroCelular=num;
 	}
 	
 	public int getPosicionFaltante() {
@@ -23,30 +22,12 @@ public class Equipo {
 		this.posicionFaltante = posicionFaltante;
 	}
 	
-	public ArrayList<String> getMensajesRecibidos() {
-		return this.mensajesRecibidos;
-	}
-
-	public void setMensajesRecibidos(ArrayList<String> mensajesRecibidos) {
-		this.mensajesRecibidos = mensajesRecibidos;
-	}
-	
 	public String getNumeroCelular() {
 		return numeroCelular;
 	}
 
 	public void setNumeroCelular(String numeroCelular) {
 		this.numeroCelular = numeroCelular;
-	}
-	
-	public void recibirMensaje(String m) {
-		this.mensajesRecibidos.add(m);
-	}
-	
-	public void enviarMensaje(Jugador j) {
-		String mensaje = "Hola, nuestro equipo es "+ this.nombre;
-		mensaje += "./n Podrías jugar con nosotros./n Nuestro número de contacto es "+ this.numeroCelular;
-		j.recibirMensaje(mensaje);
 	}
 
 	
