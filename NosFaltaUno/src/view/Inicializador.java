14607e810c482aee0597a2c5e83c364f931f3f27
coniviere <43079502+coniviere@users.noreply.github.com>
2019-12-13 11:55:18 -0300

    Update init

--- NosFaltaUno/src/view/Inicializador.java
+++ NosFaltaUno/src/view/Inicializador.java
@@ -2,14 +2,12 @@ package view;
 
 import java.util.ArrayList;
 
-
 import entity.Barrio;
 import entity.Equipo;
 import entity.Jugador;
 
 public class Inicializador {
-
-	public Inicializador() {
+public Inicializador() {
 		
 	}
 	
@@ -27,10 +25,10 @@ public class Inicializador {
 		barrios.add(sanfer);
 		barrios.add(sani);
 		
-		Jugador ignacio = new Jugador("Ignacio",9);
-		Jugador juan = new Jugador("Juan",5);
-		Jugador pedro = new Jugador("Pedro",5);
-		Jugador pablo = new Jugador("Pablo",5);
+		Jugador ignacio = new Jugador("Ignacio",9, "15-6655-1111");
+		Jugador juan = new Jugador("Juan",5, "15-5557-6565");
+		Jugador pedro = new Jugador("Pedro",5, "15-4342-1188");
+		Jugador pablo = new Jugador("Pablo",5, "15-0099-6789");
 		
 		
 		pilar.addJugador(ignacio);
@@ -38,10 +36,10 @@ public class Inicializador {
 		sanfer.addJugador(pedro);
 		sanfer.addJugador(pablo);
 		
-		Equipo malaFama = new Equipo("Mala Fama",9);
-		Equipo pimienta = new Equipo("Pimienta",5);
-		Equipo losLeales = new Equipo("Los leales",5);
-		Equipo elPlata = new Equipo("El plata",5);
+		Equipo malaFama = new Equipo("Mala Fama",9, "15-5557-1111");
+		Equipo pimienta = new Equipo("Pimienta",5, "15-5007-1771");
+		Equipo losLeales = new Equipo("Los leales",5, "15-0099-6760");
+		Equipo elPlata = new Equipo("El plata",5, "15-5643-1111");
 		
 		
 		pilar.addEquipo(malaFama);
@@ -51,5 +49,6 @@ public class Inicializador {
 		return barrios;
 		
 	}
+
 
 }
